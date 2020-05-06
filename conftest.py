57e6f5c8c46d77dbf099d7735506032fc751a919
Vladimir Ol <oleynik.96@gmail.com>
2020-05-06 19:29:07 +0300

    Update card model, fixture and requirements

--- conftest.py
+++ conftest.py
@@ -1,7 +1,17 @@
 import pytest
+
+from src.models.card import Card
 from src.models.user import User
 
 
 @pytest.fixture
 def fake_user():
     return User.from_faker().get_user()
+
+
+@pytest.fixture
+def fake_card():
+    def _get_card(user_id):
+        return Card.from_faker(user_id).card
+
+    return _get_card
