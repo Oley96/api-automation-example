1048a866d06cc9f04736262fd0503627525a1994
Vladimir Ol <oleynik.96@gmail.com>
2020-05-06 19:07:36 +0300

    Refactored card model

--- src/models/card.py
+++ src/models/card.py
@@ -1,8 +1,29 @@
+from faker import Faker
+
+
 class Card:
 
     def __init__(self, longNum, expires, ccv, userID):
-        self.longNum = longNum
+        self.long_num = longNum
         self.expires = expires
         self.ccv = ccv
-        self.userID = userID
+        self.user_id = userID
 
+    @classmethod
+    def from_faker(cls, user_id):
+        data = {
+            "longNum": Faker().credit_card_number(),
+            "expires": Faker().credit_card_expire(),
+            "ccv": Faker().credit_card_security_code(),
+            "userID": user_id
+        }
+        return cls(**data)
+
+    @property
+    def card(self):
+        return {
+            "longNum": self.long_num,
+            "expires": self.expires,
+            "ccv": self.ccv,
+            "userID": self.user_id
+        }
